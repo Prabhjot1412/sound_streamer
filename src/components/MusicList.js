cbef4bb61a639be729b8a4d94d3292f1595cfd76
prabhjot <prabhjot@gmail.com>
2024-01-17 13:12:53 -0500

    play songs from list

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -6,13 +6,17 @@ import MusicForm from "./MusicForm"
 
 const MusicList = (props) => {
   const {musicData} = props
-  const [activeSong, setActiveSong] = useState({})
+  const [activeSong, setActiveSong] = useState(false)
   const [audioDisplay, setAudioDisplay] = useState(0)
   const [showAudio, setShowAudio] = useState(false)
   const [showMusicModal, setShowMusicModal] = useState(false)
 
   useEffect(() => {
-    if (!musicData) {
+    if (musicData === undefined) {
+      return
+    }
+
+    if (activeSong) {
       return
     }
 
@@ -30,13 +34,14 @@ const MusicList = (props) => {
 
   return(
     <div>
-      <div style={{padding: 30, display: "flex", flexDirection: "column", width: "90%", height: 700}} className="ml-10 rounded-lg bg-indigo-100 transition ease-in-out delay-500"
+      <div style={{padding: 30, display: "flex", flexDirection: "column", justifyContent: "space-between", width: "90%", height: 700}} className="ml-10 rounded-lg bg-indigo-100 transition ease-in-out delay-500"
         onMouseOver={() => {setShowAudio(true); setAudioDisplay(1)}}
       >
         <p className="text-4xl font-bold mb-10"> {activeSong.name}</p>
-        <img src={activeSong.thumbnail} alt="thumbnail" className="mb-5" style={{alignSelf: "center", Width: 500, maxHeight: 500}} 
-          onMouseOver={() => {setShowAudio(true); setAudioDisplay(1)}} />
-
+        { activeSong.thumbnail ?
+          <img src={activeSong.thumbnail} alt="thumbnail" className="mb-5 rounded-md" style={{alignSelf: "center", Width: 500, maxHeight: 500}} 
+            onMouseOver={() => {setShowAudio(true); setAudioDisplay(1)}} /> : null
+        }
         <audio alt="song" style={{opacity: audioDisplay}} className="w-full transition-all duration-200 ease-in-out " src={activeSong.url} controls autoPlay/>
       </div>
 
@@ -59,7 +64,7 @@ const MusicList = (props) => {
       <div style={{padding: 5}}>
         {musicData && musicData.map((music) => {
           return(
-            <div className="rounded-lg bg-cyan-100 mb-5 mt-2 flex" style={{justifyContent: "space-between"}}>
+            <div className={`${activeSong === music ? "bg-cyan-200 border-4 border-indigo-200" : "bg-cyan-100"} rounded-lg mb-5 mt-2 flex`} style={{justifyContent: "space-between"}}>
                 <div className="flex">
                   {music.thumbnail &&
                     <img className="rounded-md mr-5" src={music.thumbnail} style={{height: 50, width: 50}}/>
@@ -69,9 +74,11 @@ const MusicList = (props) => {
                   </span>
                 </div>
 
-                <div className="bg-cyan-100" style={{padding: "10px", paddingTop: 12}}>
+                <button className={`transition-all duration-200 hover:text-white bg-cyan-100 hover:bg-cyan-300`} style={{padding: "10px", paddingTop: 12}}
+                  onClick={() => setActiveSong(music)}
+                >
                   <Play w="6" h="6" />
-                </div>
+                </button>
             </div>
           )
         })}
